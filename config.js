5ec5293a528453456f9e1e89ccdf52f74cdae0d0
TH3 F9M0US B0Y ARAV H3R3 <153765611+rahilpanchal@users.noreply.github.com>
2024-11-25 09:44:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,11 +6,11 @@ import fs from 'fs'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const defaultOwner = '923444844060';
+const defaultOwner = '917017659124';
 
 
 // Check for the OWNERS environment variable; if not found, use the default
-const ownervb = process.env.OWNERS || process.env.OWNER_NUMBER || 'your number';  // put your number here
+const ownervb = process.env.OWNERS || process.env.OWNER_NUMBER || '917017659124';  // put your number here
 
 const ownerlist = ownervb.split(';');
 
@@ -19,9 +19,9 @@ for (let i = 0; i < ownerlist.length; i++) {
     global.owner.push([ownerlist[i], true]);
 }
 //
-global.botname = process.env.BOTNAME || 'ULTRA-MD';
-global.pairingNumber = process.env.BOT_NUMBER || 'your number';  // put your number here
-global.SESSION_ID = process.env.SESSION_ID || 'session id';  // put your session id here
+global.botname = process.env.BOTNAME || 'RAHUL PANCHAL VISHAWAKARMA';
+global.pairingNumber = process.env.BOT_NUMBER || '917017659124';  // put your number here
+global.SESSION_ID = process.env.SESSION_ID || 'aJYlASiZ#-OnGhkutUTINUWaP0IcIB09We85HliWY3uQAG_bHQQA';  // put your session id here
 
 global.mods = []
 global.prems = []
